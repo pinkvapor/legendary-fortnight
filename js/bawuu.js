ad8d8e55a10dc2915766ec8a51df091931913478
ACE_E303 <ace_e303@e303-s03.lab.lagcc>
2017-06-09 16:45:30 -0400

    added close function

--- js/bawuu.js
+++ js/bawuu.js
@@ -1,19 +1,28 @@
 var userSubmit = document.querySelector('#user-submit');
+var toDoList = document.querySelector('#to-do-list');
 
 function addToDo(event) {
 	event.preventDefault();
 
 	var userInput = document.querySelector('#user-input');
-	var toDoList = document.querySelector('#to-do-list');
-
+	
 	if(userInput.value === '') {
 		return false;
 	}
 
-	toDoList.innerHTML += '<li>' + userInput.value + '</li>';
+	toDoList.innerHTML = '<li><i class="fa fa-window-close close-to-do" aria-hidden="true"></i>' + userInput.value + '</li>' + toDoList.innerHTML;
 
 	userInput.value = '';
 
 }
 
+function removeToDo(event){
+	if (event.target.classList.contains('close-to-do')) {
+		var li = event.target.parentElement;
+		toDoList.removeChild(li);
+		console.log(li);
+	}
+}
+
+toDoList.addEventListener('click', removeToDo, false);
 userSubmit.addEventListener('click', addToDo, false);
